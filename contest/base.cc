63f057205470f579da9a552fc8f2ef83423e824b
Ugur Yagmur <cuy.official@gmail.com>
2022-12-28 22:32:49 +0000

    cp contest setup is added

---
+++ contest/base.cc
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <vector>
+
+#ifdef DEBUG
+#include "debug.cc"
+#else
+#define debug(...) ""
+#endif
+
+#define print_cont(arr)                                                        \
+  for (size_t i = 0; i < arr.size(); i++)                                      \
+    std::cout << arr[i] << ' ';                                                \
+  std::cout << std::endl
+
+#define print_cont_reverse(arr)                                                \
+  for (size_t i = arr.size() - 1; i >= 0; i--)                                 \
+    std::cout << arr[i] << ' ';                                                \
+  std::cout << std::endl
+
+using i = int;
+using ll = long long;
+using s = std::string;
+using vi = std::vector<i>;
+using vll = std::vector<ll>;
+using vs = std::vector<s>;
+
+void read_array(size_t size, std::vector<long long> &arr) {
+  for (size_t i = 0; i < size; i++) {
+    std::cin >> arr[i];
+  }
+}
+
+void read_array(size_t size, long long *arr[]) {
+  for (size_t i = 0; i < size; i++) {
+    std::cin >> *arr[i];
+  }
+}
+
+void solve(int test_num) {}
+
+int main() {
+  int test_num;
+  std::cin >> test_num;
+  for (int i = 1; i <= test_num; i++) {
+    solve(i);
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ contest/base.py
@@ -0,0 +1,7 @@
+solve(test_num: int) -> None:
+    pass
+
+if __name__ == "__main__":
+    test_num = int(input())
+    for i in range(1, test_num+1):
+        solve(test_num)
