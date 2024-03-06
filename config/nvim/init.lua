d9b711f50991038e0ad52979f3e7d60fbba2b12d
Ugur Yagmur <cuy.official@gmail.com>
2024-03-06 17:04:24 +0000

    fixing nvim autocomplete suggestion issue

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -18,6 +18,8 @@ vim.opt.termguicolors = true
 -- opts  -> options as lua table
 vim.g.mapleader = ' '
 vim.keymap.set("n", "<leader>q", ":q!<CR>", {})
+vim.keymap.set("n", "<leader>n", ":bn<CR>", {})
+vim.keymap.set("n", "<leader>N", ":bp<CR>", {})
 
 
 -- lazy package manager for vim
@@ -60,7 +62,11 @@ lazy.setup({
 	{'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }},
 	{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
 	{'neovim/nvim-lspconfig'},
-	{'Mofiqul/vscode.nvim'}
+	{'Mofiqul/vscode.nvim'},
+	{'hrsh7th/nvim-cmp'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/cmp-vsnip'},
+	{'hrsh7th/vim-vsnip'}
 })
 
 -- colorscheme configuration
@@ -172,4 +178,80 @@ require('lspconfig').rust_analyzer.setup{
       }
     }
   }
+}
+
+
+-- set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
+
+-- Set up autocomplete for lspconfig
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['clangd'].setup {
+	capabilities = capabilities
+}
+require('lspconfig')['pylsp'].setup {
+	capabilities = capabilities
+}
+require('lspconfig')['rust_analyzer'].setup {
+	capabilities = capabilities
 }
