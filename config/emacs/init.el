84f16e5724517355415d093851911f90bf8e7a66
Ugur Yagmur <cuy.official@gmail.com>
2024-07-13 16:36:06 +0100

    remove nvim and add emacs configs

---
+++ config/emacs/init.el
@@ -0,0 +1,36 @@
+(setq inhibit-startup-message t) ; Don't show the splash screen
+(setq visible-bell t)            ; Flash when the bell rings
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; visual configurations
+(global-display-line-numbers-mode 1)
+(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
+(require 'vscode-dark-plus-theme "~/.config/emacs/themes/vscode-dark-plus-theme.el")
+(load-theme 'vscode-dark-plus t)
+(set-frame-font "Monospace 20" nil t)
+
+
+;; functional configurations
+(require 'evil)
+(evil-mode 1)
+
+(require 'company)
+(company-mode t)
+
+(require 'eglot)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
